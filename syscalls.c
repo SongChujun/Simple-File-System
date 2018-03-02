850f0a10ff46006c82f7e2dabd85771923ccbed6
SongChujun <a925504340@gmail.com>
2018-03-02 18:07:38 +0800

    shell with mkdir & cd

--- syscalls.c
+++ syscalls.c
@@ -5,6 +5,7 @@
 //
 void FuncCreat(const char* path)
 {
+    formalizecmdline(path);
     char dirname[16];
     int begin=0;
     for(begin=strlen(path)-1;((path[begin]!='/')&&(begin>0));begin--);
@@ -21,6 +22,7 @@ void FuncCreat(const char* path)
     while(1)
     {
         char tmpname[16];
+        chararrayclear(tmpname,16);
         int j;
         for(j=i+1;;j++)
         {
@@ -32,10 +34,8 @@ void FuncCreat(const char* path)
             {
                 break;
             }
-            tmpname[j-i-1]=path[i];
+            tmpname[j-i-1]=path[j];
         }
-        i=j;
-        flag=0;
         if((strcmp(tmpname,dirname)==0)&&(recordbegin==i))
         {
             struct TreeNode* newnode=(struct TreeNode*)malloc(sizeof(struct TreeNode));
@@ -49,15 +49,21 @@ void FuncCreat(const char* path)
             freenode[freeinodenum]=1;
             struct inode newinode;
             newinode.length=0;
-                                                                                                        
+            newinode.type=0;
+            newinode.storetype=-1;
+            intarrayclear(newinode.i_addr,8);
+            fseek(fp,SEEK_SET,*512+freeinodenum*sizeof(struct inode));
+            fwrite(&newinode,sizeof(newinode),1,fp);
         }
+        i=j;
+        flag=0;
         for(int k=0;k<curnode->ChildNum;k++)
         {
-            if(strcmp(curnode->name,tmpname)==0)
+            if(strcmp(curnode->childlist[k]->name,tmpname)==0)
             {
                 flag=1;
-                curnode=curnode->childlist[curnode->ChildNum];
-                if(curnode->type==0)
+                curnode=curnode->childlist[k];
+                if(curnode->childlist[k]->type==0)
                 {
                     printf("Invalid Path\n");
                     return ;
@@ -72,8 +78,9 @@ void FuncCreat(const char* path)
         }
     }
 }
-int FuncOpen(const char* path)
+int FuncOpen(const char* path,int mode)
 {
+    formalizecmdline(path);
     char filename[16];
     int begin=0;
     for(begin=strlen(path)-1;((path[begin]!='/')&&(begin>0));begin--);
@@ -90,6 +97,7 @@ int FuncOpen(const char* path)
     while(1)
     {
         char tmpname[16];
+        chararrayclear(tmpname,16);
         int j;
         for(j=i+1;;j++)
         {
@@ -101,41 +109,71 @@ int FuncOpen(const char* path)
             {
                 break;
             }
-            tmpname[j-i-1]=path[i];
+            tmpname[j-i-1]=path[j];
+        }
+        if((strcmp(tmpname,filename)==0)&&(recordbegin==i))
+        {
+            for(int k=0;k<curnode->ChildNum;k++)
+            {
+                if(strcmp(curnode->childlist[k]->name,tmpname)==0)
+                {
+                    flag=1;
+                    curnode=curnode->childlist[k];
+                    if(curnode->childlist[k]->type==0)
+                    {
+                        printf("Invalid Path\n");
+                        return -1;
+                    }
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                printf("The file doesn't exist!\n");
+                return -1;
+            }
+            if(mode==1)
+            {
+                curnode->statue=1;
+                struct inode newinode;
+                fseek(fp,SEEK_SET,512*20+(curnode->inodenum)*sizeof(struct inode));
+                fread(&newinode,sizeof(struct inode),1,fp);
+                struct  activeinode *newactiveinode=(struct activeinode*)malloc(sizeof(struct activeinode));
+                newactiveinode->inodenum=curnode->inodenum;
+                newactiveinode->type=1;
+                strcpy(newactiveinode->i_addr,newinode.i_addr);
+                newactiveinode->length=newinode.length;
+                newactiveinode->1=1;
+                int fd=computefreesheetnum();
+                curnode->filedescriptor=fd;
+                freesystemopenfilesheet[fd]=1;
+                u_ofile[fd]=(struct systemopenfilesheet*)malloc(sizeof(struct systemopenfilesheet));
+                u_ofile[fd]->offset=0;
+                u_ofile[fd]->ptrtoactiveinode=newactiveinode;
+                u_ofile[fd]->refcount=1;
+                return fd;
+            }
+            else
+            {
+                free(u_ofile[curnode->filedescriptor]->ptrtoactiveinode);
+                free(u_ofile[curnode->filedescriptor]);
+                freesystemopenfilesheet[curnode->filedescriptor]=0;
+                //成组链接删除文件
+                (curnode->father->ChildNum)-1;
+                freeinodenum[curnode->inodenum]=1;
+                free(curnode);
+
+            }
         }
         i=j;
         flag=0;
-        if((strcmp(tmpname,filename)==0)&&(recordbegin==i))
-        {
-            struct TreeNode* newnode=(struct TreeNode*)malloc(sizeof(struct TreeNode));
-            strcpy(newnode->name,filename);
-            newnode->father=curnode;
-            newnode->type=0;
-            newnode->ChildNum=0;
-            curnode->childlist[(curnode->ChildNum)++]=newnode;
-
-            freeinodenum=computeinodenum();
-            curnode->inodenum=freeinodenum;
-            freenode[freeinodenum]=1;
-
-            struct inode newinode;
-            newinode.length=0;
-
-            struct activeinode* actvinode=(struct activeinode*)malloc(sizeof(actvinode));
-            actvinode->length=0;
-            freesystemopenfilesheet[computefreesheetnum()]=1;
-            u_ofile[computefreesheetnum()]=(struct systemopenfilesheet*)malloc(sizeof(struct systemopenfilesheet));
-            u_ofile[computefreesheetnum()]->offset=0;
-            u_ofile[computefreesheetnum()]->ptrtoactiveinode=actvinode;
-            u_ofile[computefreesheetnum()]->refcount=1;
-        }
         for(int k=0;k<curnode->ChildNum;k++)
         {
-            if(strcmp(curnode->name,tmpname)==0)
+            if(strcmp(curnode->childlist[k]->name,tmpname)==0)
             {
                 flag=1;
-                curnode=curnode->childlist[curnode->ChildNum];
-                if(curnode->type==0)
+                curnode=curnode->childlist[k];
+                if(curnode->childlist[k]->type==0)
                 {
                     printf("Invalid Path\n");
                     return -1;
